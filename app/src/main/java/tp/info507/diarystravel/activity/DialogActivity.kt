35483b50721098ffeb8e7deb8229262ae7fab55d
Cyril Pitar <98352804+AyRron@users.noreply.github.com>
2023-11-06 14:07:01 +0100

    ???

--- app/src/main/java/tp/info507/diarystravel/activity/DialogActivity.kt
+++ app/src/main/java/tp/info507/diarystravel/activity/DialogActivity.kt
@@ -6,7 +6,9 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import tp.info507.diarystravel.R
+import tp.info507.diarystravel.model.Travel
 import tp.info507.diarystravel.storage.TravelDataBaseStorage
+import tp.info507.diarystravel.storage.TravelStorage
 
 class DialogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +33,20 @@ class DialogActivity : AppCompatActivity() {
             Toast.makeText(applicationContext, name.text.toString(), Toast.LENGTH_SHORT).show()
             Toast.makeText(applicationContext, date.text.toString(), Toast.LENGTH_SHORT).show()
             finish()
+        }
+    }
+
+    private fun handleTravelInsertion(nom: String, date: String, localisation: String) {
+        val travel = Travel(
+            0, nom, date, localisation, description = "")
+
+        val travelStorage = TravelDataBaseStorage(this)
+        val test = travelStorage.insert(travel)
+
+        if (test != -1){
+            Toast.makeText(applicationContext, "Insertion réussie ! ;)", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(applicationContext, "Insertion pas réussie ! :(", Toast.LENGTH_SHORT).show()
         }
     }
 }
